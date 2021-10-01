0779bb813a6bff250f8f1614995fc58c8d9fa056
bigelowd-erau <bigelowd@my.erau.edu>
2021-10-01 00:05:53 -0700

    Trimmed Code

--- Cubethon++/Assets/Scripts/Client.cs
+++ Cubethon++/Assets/Scripts/Client.cs
@@ -8,14 +8,9 @@ public class Client : MonoBehaviour
     public delegate void PlayerCommandInput(Command command);
     public static event PlayerCommandInput OnPlayerCommandInput;
 
-    public delegate void ApplyPlayerCommand();
-    public static event ApplyPlayerCommand ExecutePlayerCommand;
-
     private void OnEnable()
     {
         m_PlayerReciever = new PlayerReciever(GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>());
-        //Debug.Log(GameObject.FindGameObjectsWithTag("Player").Length);
-        //m_PlayerReciever = new PlayerReciever(pm);
         PlayerCollision.OnHitObstacle += Disable;
     }
 
@@ -24,33 +19,17 @@ public class Client : MonoBehaviour
         if (Input.GetKey("a"))
         {
             Command playerCommand = new MoveLeftCommand(m_PlayerReciever);
-            //Invoker invoker = new Invoker();
-
             OnPlayerCommandInput(playerCommand);
-
-            //invoker.SetCommand(playerCommand);
-            //invoker.ExecuteCommand();
         }
         if (Input.GetKey("d"))
         {
             Command playerCommand = new MoveRightCommand(m_PlayerReciever);
-            //Invoker invoker = new Invoker();
             OnPlayerCommandInput(playerCommand);
-
-            //invoker.SetCommand(playerCommand);
-            //invoker.ExecuteCommand();
         }
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        //ExecutePlayerCommand();
     }
 
     public void Disable()
     {
-        //this.enabled = false;
         Destroy(this);
     }
 
--- Cubethon++/Assets/Scripts/Invoker.cs
+++ Cubethon++/Assets/Scripts/Invoker.cs
@@ -5,23 +5,15 @@ using System.Collections.Generic;
 public class Invoker: MonoBehaviour
 {
     private Command m_Command;
-    //public Queue<Command> commandQueue;
-    //public CommandDisplay commandDisplay;
-
-    public delegate void OnCommandExecution(Command m_Command);
-    public static event OnCommandExecution CommandExecuted;
 
     public void OnEnable()
     {
-        //commandQueue = new Queue<Command>();
         Client.OnPlayerCommandInput += SetCommand;
-        //Client.ExecutePlayerCommand += ExecuteCommand;
     }
 
     public void SetCommand(Command command)
     {
         m_Command = command;
-        //commandDisplay.AddCommandToDisplay(m_Command); ;
     }
 
     public void FixedUpdate()
@@ -34,7 +26,6 @@ public class Invoker: MonoBehaviour
         if (m_Command != null)
         { 
             m_Command.Execute();
-            //CommandExecuted(m_Command);
             CommandLog.commands.Enqueue(m_Command);
             m_Command = null;
         }
@@ -42,8 +33,6 @@ public class Invoker: MonoBehaviour
 
     public void OnDisable()
     {
-        //commandQueue = new Queue<Command>();
         Client.OnPlayerCommandInput -= SetCommand;
-        //Client.ExecutePlayerCommand -= ExecuteCommand;
     }
 }
